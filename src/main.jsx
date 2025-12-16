8d8f732cf7879be0bc02ef97c578978e5eea0fec
Sujal Bhagat <partimerbhindmask@gmail.com>
2025-12-16 10:28:49 -0800

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,41 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.jsx'
+import {createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './Pages/Home.jsx'
 import About from './Pages/About.jsx'
 import Skill from './Pages/Skill.jsx'
 import Project from './Pages/Project.jsx'
 import Contact from './Pages/Contact.jsx'
 
+
 const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/DeveloperPortfolioWebsite/about', element: <About /> },
-  { path: '/DeveloperPortfolioWebsite/skills', element: <Skill /> },
-  { path: '/DeveloperPortfolioWebsite/projects', element: <Project /> },
-  { path: '/DeveloperPortfolioWebsite/contact', element: <Contact /> },
+  
+  {
+    path:'/',
+    element:<Home />
+  },
+   {
+    path:'about',
+    element:<About />
+  },
+   {
+    path:'skills',
+    element:<Skill />
+  },
+   {
+    path:'projects',
+    element:<Project />
+  },
+   {
+    path:'contact',
+    element:<Contact />
+  },
 ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+<RouterProvider router={router} />
+  </StrictMode>,
 )
