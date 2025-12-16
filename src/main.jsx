07135d302b9ff97d5a46c6c15c9b0173b9123020
Sujal Bhagat <partimerbhindmask@gmail.com>
2025-12-16 10:25:06 -0800

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import {createBrowserRouter, RouterProvider, BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './Pages/Home.jsx'
 import About from './Pages/About.jsx'
 import Skill from './Pages/Skill.jsx'
 import Project from './Pages/Project.jsx'
 import Contact from './Pages/Contact.jsx'
 
-
 const router = createBrowserRouter([
-  
-  {
-    path:'/',
-    element:<Home />
-  },
-   {
-    path:'about',
-    element:<About />
-  },
-   {
-    path:'skills',
-    element:<Skill />
-  },
-   {
-    path:'projects',
-    element:<Project />
-  },
-   {
-    path:'contact',
-    element:<Contact />
-  },
+  { path: '/', element: <Home /> },
+  { path: '/DeveloperPortfolioWebsite/about', element: <About /> },
+  { path: '/DeveloperPortfolioWebsite/skills', element: <Skill /> },
+  { path: '/DeveloperPortfolioWebsite/projects', element: <Project /> },
+  { path: '/DeveloperPortfolioWebsite/contact', element: <Contact /> },
 ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
- <BrowserRouter basename="/DeveloperPortfolioWebsite">
-      <RouterProvider router={router} />
-    </BrowserRouter>
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
