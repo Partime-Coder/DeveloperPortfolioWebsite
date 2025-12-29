54cfe0e846342985456ce7384c76221acd4f8555
Sujal Bhagat <partimerbhindmask@gmail.com>
2025-12-29 01:48:55 -0800

    first commit

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,27 +1,15 @@
-import { useEffect, useState } from "react";
+import React from 'react'
 
-function BackgroundImage({ image, overlay = false, children }) {
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    const img = new Image();
-    img.src = image;
-    img.onload = () => setLoaded(true);
-  }, [image]);
-
-  return (
-    <div
-      className="w-full h-screen bg-center relative transition-opacity duration-500"
-      style={{
-        backgroundImage: loaded ? `url(${image})` : "none",
-        backgroundSize: "cover",
-        opacity: loaded ? 1 : 0,
-      }}
-    >
-      {overlay && <div className="absolute inset-0 bg-black/40" />}
-      <div className="relative z-10 w-full h-full">{children}</div>
-    </div>
-  );
+function BackgroundImage({image, overlay=false, children}) {
+    return (
+        <div className='w-full h-screen bg-cover bg-center relative' style={{ backgroundImage: `url(${image})` }} >
+            {overlay && (<div className='absolute inset-0 bg-black/40'/>)}
+            
+            <div className='relative z-10 w-full h-full'>
+                {children}
+            </div>
+        </div>
+    )
 }
 
-export default BackgroundImage;
+export default BackgroundImage
